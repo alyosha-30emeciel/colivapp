513501fa568355302afc84d2670b9c009d36a7a5
Alyosha <alyosha@30emeciel.fr>
2023-07-23 19:16:58 +0200

    - QRCode with logo

--- src/app/components/externalLink.tsx
+++ src/app/components/externalLink.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import {ExportOutlined, QrcodeOutlined} from "@ant-design/icons";
-import {Button, Popover, QRCode} from "antd";
-
+import {Button, Popover, QRCode, Space} from "antd";
+import React from "react";
 export default function ExternalLink(props:  {href: string, title: string} ) {
-    return <><Link href={props.href} target="_blank">{props.title} <ExportOutlined/></Link> <Popover content={<QRCode value={props.href} />}><Button type="link" icon={<QrcodeOutlined/>}/></Popover> </>
+    return <>
+        <Space direction="horizontal">
+            <Link href={props.href} target="_blank">{props.title} <ExportOutlined/></Link>
+            <Popover content={<QRCode size={256} icon="/logo.png" value={props.href} />}><Button type="link" icon={<QrcodeOutlined/>}/></Popover>
+        </Space></>
 }
